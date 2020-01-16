12f221eff89c475f66326b37d926e131e7870eb9
Wyzzard123 <32074198+Wyzzard123@users.noreply.github.com>
2020-01-16 12:21:54 +0800

    Added ability to set quiz topics and no of questions/choices through a web form.

--- app.py
+++ app.py
@@ -5,20 +5,43 @@ app = Flask(__name__)
 
 @app.route('/', methods = ['GET', 'POST'])
 def index():
-    return redirect('/quiz')
+    return redirect('/quiz_settings')
 
 @app.route('/quiz', methods = ['GET', 'POST'])
 def quiz():
+    """Randomly generates quizzes and shows the answer/score after submitting."""
     if request.method == 'GET':
-        mapped_questions = generate_mapped_questions_from_file(topic="nature", infile="question_answer_bank.json", no_of_questions=6, no_of_choices=4)
-        print(mapped_questions)
+        # If there is a settings page, go through "try"
         
-        temp_json_file = "temporary_question_answer_bank.json"
+        try:
+            settings_file = "quiz_settings.json"
 
-        with open(temp_json_file, "w") as tempfile:
-            json.dump(mapped_questions, tempfile)
+            with open(settings_file, "r") as settingsfile:
+                settings = json.load(settingsfile)
+            topic = settings['topic']
+            no_of_questions = int(settings['no_of_questions'])
+            no_of_choices = int(settings['no_of_choices'])
 
-        return render_template('quiz_new.html', mapped_questions = mapped_questions)
+            mapped_questions = generate_mapped_questions_from_file(topic=topic, infile="question_answer_bank.json", no_of_questions=no_of_questions, no_of_choices=no_of_choices)
+            
+            
+            temp_json_file = "temporary_question_answer_bank.json"
+
+            with open(temp_json_file, "w") as tempfile:
+                json.dump(mapped_questions, tempfile)
+
+            return render_template('quiz_new.html', mapped_questions = mapped_questions)   
+        
+        # Otherwise (eg if the website is accessed for the first time without going through the settings page), go through the default parameters in "except"
+        except:
+            mapped_questions = generate_mapped_questions_from_file(topic="nature", infile="question_answer_bank.json", no_of_questions=6, no_of_choices=4)
+            
+            temp_json_file = "temporary_question_answer_bank.json"
+
+            with open(temp_json_file, "w") as tempfile:
+                json.dump(mapped_questions, tempfile)
+
+            return render_template('quiz_new.html', mapped_questions = mapped_questions)
     elif request.method == 'POST':
         if request.form["quiz_submit_button"]:
             # Used to check what answers were given
@@ -54,4 +77,38 @@ def quiz():
             return render_template('quiz_submitted.html', mapped_questions = mapped_questions, correct_answers = correct_answers, no_of_correct_answers = no_of_correct_answers, no_of_questions = no_of_questions, correct_or_wrong_list = correct_or_wrong_list, chosen_answers = chosen_answers, chosen_answers_list=chosen_answers_list, percentage_score = percentage_score)
 
     
-    
+@app.route('/quiz_settings', methods = ['GET', 'POST'])
+def topic():
+    """Set topic, number of questions and number of choices for quiz."""
+    if request.method == 'GET':
+        infile = "question_answer_bank.json"
+
+        with open(infile) as json_file:
+            file_data = json.load(json_file)
+
+        topics = sorted(file_data.keys())
+        return render_template('quiz_settings.html', topics=topics) 
+    elif request.method == 'POST':
+        
+        if request.form["quiz_topic_button"]:
+            
+            settings = request.form.to_dict()
+            topic = settings['topic']
+            no_of_questions = int(settings['no_of_questions'])
+            no_of_choices = int(settings['no_of_choices'])
+
+            mapped_questions = generate_mapped_questions_from_file(topic=topic, infile="question_answer_bank.json", no_of_questions=no_of_questions, no_of_choices=no_of_choices)
+            
+            
+            temp_json_file = "temporary_question_answer_bank.json"
+
+            with open(temp_json_file, "w") as tempfile:
+                json.dump(mapped_questions, tempfile)
+
+            settings_file = "quiz_settings.json"
+
+            with open(settings_file, "w") as settingsfile:
+                json.dump(settings, settingsfile)
+
+            return render_template('quiz_new.html', mapped_questions = mapped_questions)   
+            #TODO create new json file for different users
